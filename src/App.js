f2f99ac8b44f3b968b885e92c356c0c60b6c1219
malla-lokesh <121810307046@gitam.in>
2023-02-27 20:45:23 +0530

    Added date functionality

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ function App() {
 
     const expenses = [
         {
+            date: new Date(2023, 2, 27),
             expenseItem: 'Food',
             expensePrice: 10,
             locationOfExpenditure: 'Shop'
         },
         {
+            date: new Date(2022, 8, 17),
             expenseItem: 'Petrol',
             expensePrice: 100,
             locationOfExpenditure: 'Bunk'
         },
         {
+            date: new Date(2021, 10, 4),
             expenseItem: 'Movie',
             expensePrice: 200,
             locationOfExpenditure: 'Theater'
         },
         {
+            date: new Date(2022, 11, 17),
             expenseItem: 'Exams',
             expensePrice: 500,
             locationOfExpenditure: 'University'
@@ -32,6 +36,7 @@ function App() {
             {
                 expenses.map((expense) => (
                     <ExpenseItem
+                        date = {expense.date}
                         expenseItem = {expense.expenseItem}
                         expensePrice = {expense.expensePrice}
                         locationOfExpenditure = {expense.locationOfExpenditure}
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,15 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
     return (
-        <div>
-            <div className='expense-item'>
-                <div className='expense-item__description'>
-                    <h2>{props.expenseItem}</h2>
-                    <h2>{props.locationOfExpenditure}</h2>
-                    <div className='expense-item__price'>
-                        Rs {props.expensePrice}
-                    </div>
+        <div className='expense-item'>
+            <ExpenseDate date={props.date}/>
+            <div className='expense-item__description'>
+                <h2>{props.expenseItem}</h2>
+                <h2>{props.locationOfExpenditure}</h2>
+                <div className='expense-item__price'>
+                    Rs {props.expensePrice}
                 </div>
             </div>
         </div>
