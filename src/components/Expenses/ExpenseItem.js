9cac1672ffdccdb1bc39f74584c868575e2f5821
malla-lokesh <121810307046@gitam.in>
2023-03-01 21:20:33 +0530

    Added updating ExpenseItem functionality using states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,30 @@
+import React, {useState} from 'react'
+
 import './ExpenseItem.css'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
 
 const ExpenseItem = (props) => {
 
-    const deleteHandler = () => {
-        document.getElementById(props.id).remove()
+    const [expenseItem, setExpenseItem] = useState(props.expenseItem)
+
+    const updateHandler = () => {
+        setExpenseItem('Updated')
+        console.log(expenseItem)
     }
 
     return (
-        <div id={props.id}>
-            <Card className='expense-item'>
-                <ExpenseDate date={props.date}/>
-                <div className='expense-item__description'>
-                    <h2>{props.expenseItem}</h2>
-                    <h2>{props.locationOfExpenditure}</h2>
-                    <div className='expense-item__price'>
-                        Rs {props.expensePrice}
-                    </div>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}/>
+            <div className='expense-item__description'>
+                <h2>{expenseItem}</h2>
+                <h2>{props.locationOfExpenditure}</h2>
+                <div className='expense-item__price'>
+                    Rs {props.expensePrice}
                 </div>
-                <button onClick={deleteHandler}>Delete Expense</button>
-            </Card>
-        </div>
+            </div>
+            <button onClick={updateHandler}>Update Expense</button>
+        </Card>
     )
 }
 
