1b9ff0d807cc442a86af884cab2bfc1453ecf275
malla-lokesh <121810307046@gitam.in>
2023-03-03 11:29:41 +0530

    Added multi state functionality

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,19 +1,35 @@
+import { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
 
-    const printTitle = (event) => {
-        console.log(event.target.value)
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+        console.log(enteredTitle)
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+        console.log(enteredAmount)
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+        console.log(enteredDate)
     }
 
     return <div className="form">
         <form>
             <label>Title</label>
-            <input type='Text' onChange={printTitle}/>
+            <input type='Text' onChange={titleChangeHandler}/>
             <label>Amount spent:</label>
-            <input type='number'/>
+            <input type='number' onChange={amountChangeHandler} />
             <label>Date:</label>
-            <input type='Date'/>
+            <input type='Date' onChange={dateChangeHandler}/>
             <button type="submit">Submit</button>
         </form>
     </div>
