60a6066ab5f7f667e0a29e636ae19793f89a3b4a
malla-lokesh <121810307046@gitam.in>
2023-03-14 17:06:45 +0530

    Rendering Expenses with performance Optimizations

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,61 +3,66 @@ import './Expenses.css'
 import Card from "../UI/Card"
 import NewExpense from "../NewExpense/NewExpense"
 import { useState } from "react"
+import ExpenseFilter from "./ExpenseFilter"
+
+const dummy_expenses = [
+    {
+        id : 1,
+        date: new Date(2023, 1, 27),
+        expenseItem: 'Food',
+        expensePrice: 10,
+        locationOfExpenditure: 'Shop'
+    },
+    {
+        id : 2,
+        date: new Date(2022, 7, 21),
+        expenseItem: 'Petrol',
+        expensePrice: 100,
+        locationOfExpenditure: 'Bunk'
+    },
+    {
+        id : 3,
+        date: new Date(2021, 9, 4),
+        expenseItem: 'Movie',
+        expensePrice: 200,
+        locationOfExpenditure: 'Theater'
+    },
+    {
+        id : 4,
+        date: new Date(2022, 10, 17),
+        expenseItem: 'Exams',
+        expensePrice: 500,
+        locationOfExpenditure: 'University'
+    }
+]
 
 const Expenses = () => {
-    const [expenses, setExpenses] = useState([
-        {
-            id : 1,
-            date: new Date(2023, 1, 27),
-            expenseItem: 'Food',
-            expensePrice: 10,
-            locationOfExpenditure: 'Shop'
-        },
-        {
-            id : 2,
-            date: new Date(2022, 7, 21),
-            expenseItem: 'Petrol',
-            expensePrice: 100,
-            locationOfExpenditure: 'Bunk'
-        },
-        {
-            id : 3,
-            date: new Date(2021, 9, 4),
-            expenseItem: 'Movie',
-            expensePrice: 200,
-            locationOfExpenditure: 'Theater'
-        },
-        {
-            id : 4,
-            date: new Date(2022, 10, 17),
-            expenseItem: 'Exams',
-            expensePrice: 500,
-            locationOfExpenditure: 'University'
-        }
-    ])
+    const [expenses, setExpenses] = useState(dummy_expenses)
 
     const newExpenseData = expenseData => {
         setExpenses((prevExpenses) => {
-            return [...prevExpenses, expenseData]
+            return [expenseData, ...prevExpenses]
         })
     }
 
     return(
-        <Card className="expenses">
-            <h2>Expense Items:</h2>
+        <div>
             <NewExpense onGettingData={newExpenseData}/>
-            {
-                expenses.map((expense) => (
-                    <ExpenseItem 
-                        id = {expense.id}
-                        date = {expense.date}
-                        expenseItem = {expense.expenseItem}
-                        expensePrice = {expense.expensePrice}
-                        locationOfExpenditure = {expense.locationOfExpenditure}
-                    />
-                ))
-            }
-        </Card>
+            <Card className="expenses">
+                <ExpenseFilter />
+                {
+                    expenses.map((expense) => (
+                        <ExpenseItem 
+                            key = {expense.id}
+                            date = {expense.date}
+                            expenseItem = {expense.expenseItem}
+                            expensePrice = {expense.expensePrice}
+                            locationOfExpenditure = {expense.locationOfExpenditure}
+                        />
+                    ))
+                }
+            </Card>
+        </div>
     )
 }
 
