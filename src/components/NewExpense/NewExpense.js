5752fac4bc7acf2b44e9e16799e3e359ba4f63f4
malla-lokesh <121810307046@gitam.in>
2023-03-15 11:48:27 +0530

    Challenge on Conditional Rendering

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  
+  const [form, setForm] = useState(false)
+  
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setForm(false)
   };
+
+  const addNewExpenseHandler = () => {
+    setForm(true)
+  }
+
+  const cancelForm = () => {
+    setForm(false)
+  }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!form && (
+        <button onClick={addNewExpenseHandler}>Add New Expense</button>
+      )}
+      {form && (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}
+                     onCancel={cancelForm} />
+      )}
     </div>
   );
 };
